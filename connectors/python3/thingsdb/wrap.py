6f24a9eba26578a8be7b5de2988696d08f8b0338
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-03-20 14:05:51 +0100

    Repr

--- connectors/python3/thingsdb/wrap.py
+++ connectors/python3/thingsdb/wrap.py
@@ -27,7 +27,7 @@ def wrap(value, blobs):
         blobs.append(blob)
         return Wrap(f'blob({idx}')
     if isinstance(value, dict):
-        return Wrap(f"{{{','.join(f'{k}:{wrap(v, blobs)}' for k, v in value.items())}}}")  # nopep8
+        return Wrap(f"{{{','.join(f'{k}:{repr(wrap(v, blobs))}' for k, v in value.items())}}}")  # nopep8
     if isinstance(value, (list, tuple)):
         return Wrap(f"[{','.join(repr(wrap(v, blobs)) for v in value)}]")
     if isinstance(value, Wrap.nowrap()):

56be7f5e2096c80c2707690e35f87c1d1b4f0547
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-10-30 11:41:18 +0100

    start work on env vars

--- src/ti/cfg.c
+++ src/ti/cfg.c
@@ -17,124 +17,17 @@
 #include <util/fx.h>
 
 static ti_cfg_t * cfg;
-static const char * ti__cfg_section = "thingsdb";
+static const char * cfg__section = "thingsdb";
 
-static int ti__cfg_storage_path(cfgparser_t * parser, const char * cfg_file);
-static void ti__cfg_port(
-        cfgparser_t * parser,
-        const char * cfg_file,
-        const char * option_name,
-        uint16_t * port);
-static void ti__cfg_zone(
-        cfgparser_t * parser,
-        const char * cfg_file,
-        uint8_t * zone);
-static void ti__cfg_ip_support(cfgparser_t * parser, const char * cfg_file);
-static int ti__cfg_str(
-        cfgparser_t * parser,
-        const char * cfg_file,
-        const char * option_name,
-        char ** str);
-static void ti__cfg_threshold_full_storage(
-        cfgparser_t * parser,
-        const char * cfg_file);
 
-int ti_cfg_create(void)
-{
-    cfg = malloc(sizeof(ti_cfg_t));
-    if (!cfg)
-        return -1;
-
-    /* set defaults */
-    cfg->client_port = TI_DEFAULT_CLIENT_PORT;
-    cfg->node_port = TI_DEFAULT_NODE_PORT;
-    cfg->http_status_port = TI_DEFAULT_HTTP_STATUS_PORT;
-    cfg->threshold_full_storage = TI_DEFAULT_THRESHOLD_FULL_STORAGE;
-    cfg->ip_support = AF_UNSPEC;
-    cfg->bind_client_addr = strdup("127.0.0.1");
-    cfg->bind_node_addr = strdup("127.0.0.1");
-    cfg->storage_path = strdup("/var/lib/thingsdb/");
-    cfg->pipe_client_name = NULL;
-    cfg->zone = 0;
-
-    if (!cfg->bind_client_addr || !cfg->bind_node_addr || !cfg->storage_path)
-        ti_cfg_destroy();
-
-    ti()->cfg = cfg;
-    return 0;
-}
-
-void ti_cfg_destroy(void)
-{
-    if (!cfg)
-        return;
-    free(cfg->bind_client_addr);
-    free(cfg->bind_node_addr);
-    free(cfg->pipe_client_name);
-    free(cfg->storage_path);
-    free(cfg);
-    cfg = ti()->cfg = NULL;
-}
-
-int ti_cfg_parse(const char * cfg_file)
-{
-    int rc;
-    cfgparser_t * parser = cfgparser_create();
-    if (!parser)
-    {
-        printf("allocation error\n");
-        return -1;
-    }
-
-    rc = cfgparser_read(parser, cfg_file);
-    if (rc != CFGPARSER_SUCCESS)
-    {
-        /* we could choose to continue with defaults but this is probably
-         * not what users want so lets quit.
-         */
-        printf("cannot not read `%s` (%s)\n", cfg_file, cfgparser_errmsg(rc));
-        rc = -1;
-        goto exit_parse;
-    }
-
-    if (    (rc = ti__cfg_storage_path(parser, cfg_file)) ||
-            (rc = ti__cfg_str(
-                    parser,
-                    cfg_file,
-                    "bind_client_addr",
-                    &cfg->bind_client_addr)) ||
-            (rc = ti__cfg_str(
-                    parser,
-                    cfg_file,
-                    "bind_node_addr",
-                    &cfg->bind_node_addr)) ||
-            (rc = ti__cfg_str(
-                    parser,
-                    cfg_file,
-                    "pipe_client_name",
-                    &cfg->pipe_client_name)))
-        goto exit_parse;
-
-    ti__cfg_port(parser, cfg_file, "listen_client_port", &cfg->client_port);
-    ti__cfg_port(parser, cfg_file, "listen_node_port", &cfg->node_port);
-    ti__cfg_port(parser, cfg_file, "http_status_port", &cfg->http_status_port);
-    ti__cfg_zone(parser, cfg_file, &cfg->zone);
-    ti__cfg_ip_support(parser, cfg_file);
-    ti__cfg_threshold_full_storage(parser, cfg_file);
-
-exit_parse:
-    cfgparser_destroy(parser);
-    return rc;
-}
-
-static int ti__cfg_storage_path(cfgparser_t * parser, const char * cfg_file)
+static int cfg__storage_path(cfgparser_t * parser, const char * cfg_file)
 {
     const char * option_name = "storage_path";
     char * storage_path;
     cfgparser_option_t * option;
     cfgparser_return_t rc;
     size_t len;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, option_name);
+    rc = cfgparser_get_option(&option, parser, cfg__section, option_name);
     if (rc != CFGPARSER_SUCCESS)
     {
         log_warning(
@@ -195,7 +88,7 @@ static int ti__cfg_storage_path(cfgparser_t * parser, const char * cfg_file)
     return 0;
 }
 
-static void ti__cfg_port(
+static void cfg__port(
         cfgparser_t * parser,
         const char * cfg_file,
         const char * option_name,
@@ -206,7 +99,7 @@ static void ti__cfg_port(
 
     cfgparser_option_t * option;
     cfgparser_return_t rc;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, option_name);
+    rc = cfgparser_get_option(&option, parser, cfg__section, option_name);
 
     if (rc != CFGPARSER_SUCCESS)
     {
@@ -238,7 +131,7 @@ static void ti__cfg_port(
     *port = (uint16_t) option->val->integer;
 }
 
-static void ti__cfg_zone(
+static void cfg__zone(
         cfgparser_t * parser,
         const char * cfg_file,
         uint8_t * zone)
@@ -248,7 +141,7 @@ static void ti__cfg_zone(
 
     cfgparser_option_t * option;
     cfgparser_return_t rc;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, "zone");
+    rc = cfgparser_get_option(&option, parser, cfg__section, "zone");
 
     if (rc != CFGPARSER_SUCCESS)
     {
@@ -279,12 +172,12 @@ static void ti__cfg_zone(
 }
 
 
-static void ti__cfg_ip_support(cfgparser_t * parser, const char * cfg_file)
+static void cfg__ip_support(cfgparser_t * parser, const char * cfg_file)
 {
     const char * option_name = "ip_support";
     cfgparser_option_t * option;
     cfgparser_return_t rc;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, option_name);
+    rc = cfgparser_get_option(&option, parser, cfg__section, option_name);
     if (rc != CFGPARSER_SUCCESS)
     {
         log_warning(
@@ -337,7 +230,7 @@ static void ti__cfg_ip_support(cfgparser_t * parser, const char * cfg_file)
             ti_tcp_ip_support_str(cfg->ip_support));
 }
 
-static int ti__cfg_str(
+static int cfg__str(
         cfgparser_t * parser,
         const char * cfg_file,
         const char * option_name,
@@ -345,7 +238,7 @@ static int ti__cfg_str(
 {
     cfgparser_option_t * option;
     cfgparser_return_t rc;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, option_name);
+    rc = cfgparser_get_option(&option, parser, cfg__section, option_name);
     if (rc != CFGPARSER_SUCCESS)
     {
         if (*str)
@@ -386,7 +279,7 @@ static int ti__cfg_str(
     return -(!*str);
 }
 
-static void ti__cfg_threshold_full_storage(
+static void cfg__threshold_full_storage(
         cfgparser_t * parser,
         const char * cfg_file)
 {
@@ -394,7 +287,7 @@ static void ti__cfg_threshold_full_storage(
 
     cfgparser_option_t * option;
     cfgparser_return_t rc;
-    rc = cfgparser_get_option(&option, parser, ti__cfg_section, option_name);
+    rc = cfgparser_get_option(&option, parser, cfg__section, option_name);
 
     if (rc != CFGPARSER_SUCCESS)
     {
@@ -421,4 +314,98 @@ static void ti__cfg_threshold_full_storage(
     }
 
     cfg->threshold_full_storage = (size_t) option->val->integer;
+}
+
+int ti_cfg_create(void)
+{
+    char * sysuser = getenv("USER");
+    char * homedir = getenv("HOME");
+
+    cfg = malloc(sizeof(ti_cfg_t));
+    if (!cfg)
+        return -1;
+
+
+    /* set defaults */
+    cfg->client_port = TI_DEFAULT_CLIENT_PORT;
+    cfg->node_port = TI_DEFAULT_NODE_PORT;
+    cfg->http_status_port = TI_DEFAULT_HTTP_STATUS_PORT;
+    cfg->threshold_full_storage = TI_DEFAULT_THRESHOLD_FULL_STORAGE;
+    cfg->ip_support = AF_UNSPEC;
+    cfg->bind_client_addr = strdup("127.0.0.1");
+    cfg->bind_node_addr = strdup("127.0.0.1");
+    cfg->storage_path = !homedir || !sysuser || strcmp(sysuser, "root") == 0
+            ? strdup("/var/lib/thingsdb/")
+            :
+    cfg->pipe_client_name = NULL;
+    cfg->zone = 0;
+
+    if (!cfg->bind_client_addr || !cfg->bind_node_addr || !cfg->storage_path)
+        ti_cfg_destroy();
+
+    ti()->cfg = cfg;
+    return 0;
+}
+
+void ti_cfg_destroy(void)
+{
+    if (!cfg)
+        return;
+    free(cfg->bind_client_addr);
+    free(cfg->bind_node_addr);
+    free(cfg->pipe_client_name);
+    free(cfg->storage_path);
+    free(cfg);
+    cfg = ti()->cfg = NULL;
+}
+
+int ti_cfg_parse(const char * cfg_file)
+{
+    int rc;
+    cfgparser_t * parser = cfgparser_create();
+    if (!parser)
+    {
+        printf("allocation error\n");
+        return -1;
+    }
+
+    rc = cfgparser_read(parser, cfg_file);
+    if (rc != CFGPARSER_SUCCESS)
+    {
+        /* we could choose to continue with defaults but this is probably
+         * not what users want so lets quit.
+         */
+        printf("cannot not read `%s` (%s)\n", cfg_file, cfgparser_errmsg(rc));
+        rc = -1;
+        goto exit_parse;
+    }
+
+    if (    (rc = cfg__storage_path(parser, cfg_file)) ||
+            (rc = cfg__str(
+                    parser,
+                    cfg_file,
+                    "bind_client_addr",
+                    &cfg->bind_client_addr)) ||
+            (rc = cfg__str(
+                    parser,
+                    cfg_file,
+                    "bind_node_addr",
+                    &cfg->bind_node_addr)) ||
+            (rc = cfg__str(
+                    parser,
+                    cfg_file,
+                    "pipe_client_name",
+                    &cfg->pipe_client_name)))
+        goto exit_parse;
+
+    cfg__port(parser, cfg_file, "listen_client_port", &cfg->client_port);
+    cfg__port(parser, cfg_file, "listen_node_port", &cfg->node_port);
+    cfg__port(parser, cfg_file, "http_status_port", &cfg->http_status_port);
+    cfg__zone(parser, cfg_file, &cfg->zone);
+    cfg__ip_support(parser, cfg_file);
+    cfg__threshold_full_storage(parser, cfg_file);
+
+exit_parse:
+    cfgparser_destroy(parser);
+    return rc;
 }

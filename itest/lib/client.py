da77d2dfc90fe59850bdbabb5246b1bead65f5ab
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-01-16 16:10:10 +0100

    Update test

--- itest/lib/client.py
+++ itest/lib/client.py
@@ -3,7 +3,9 @@ from thingsdb.client import Client
 
 async def get_client(*nodes, auth=['admin', 'pass'], **kwargs):
     client = Client(**kwargs)
+    if isinstance(auth, str):
+        auth = [auth]
     await client.connect_pool(
-        pool=[node.address_info for node in nodes],
-        auth=auth)
+        [node.address_info for node in nodes],
+        *auth)
     return client

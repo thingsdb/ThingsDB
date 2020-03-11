79a95d456ef890d1c854fac63cc23efc9e7ed213
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-03-11 16:06:57 +0100

    Remove backwards compat

--- src/ti/nodes.c
+++ src/ti/nodes.c
@@ -113,18 +113,12 @@ static void nodes__on_req_connect(ti_stream_t * stream, ti_pkg_t * pkg)
 
     mp_unp_init(&up, pkg->data, pkg->n);
 
-    /*
-     * TODO: Required, only for compatibility with ThingsDB < 0.7.10
-     *       In newer versions `mp_node_name` must always be available.
-     */
-    mp_from_node_name.tp = 0;
-
-    if (mp_next(&up, &obj) != MP_ARR || (obj.via.sz != 6 && obj.via.sz != 7) ||
+    if (mp_next(&up, &obj) != MP_ARR || obj.via.sz != 7 ||
 
         mp_next(&up, &mp_this_node_id) != MP_U64 ||
         mp_next(&up, &mp_secret) != MP_STR ||
         mp_next(&up, &mp_from_node_id) != MP_U64 ||
-        (obj.via.sz == 7 && mp_next(&up, &mp_from_node_name) != MP_STR) ||
+        mp_next(&up, &mp_from_node_name) != MP_STR ||
         mp_next(&up, &mp_version) != MP_STR ||
         mp_next(&up, &mp_min_ver) != MP_STR ||
         mp_next(&up, &obj) != MP_ARR || obj.via.sz != 7 ||
@@ -266,7 +260,7 @@ static void nodes__on_req_connect(ti_stream_t * stream, ti_pkg_t * pkg)
         msgpack_pack_uint16(&pk, ti()->cfg->node_port);   /* port */
         msgpack_pack_uint8(&pk, TI_VERSION_SYNTAX);       /* syntax version*/
 
-        goto send;
+        goto done;
     }
 
     if (this_node_id != this_node->id)
@@ -353,11 +347,8 @@ static void nodes__on_req_connect(ti_stream_t * stream, ti_pkg_t * pkg)
 
     ti_stream_set_node(stream, node);
 
-    /* update node name and/or port if required
-     * TODO: type check is only for backward compatibility with < v0.7.10
-     */
-    if (mp_from_node_name.tp == MP_STR)
-        ti_node_upd_node(node, from_node_port, &mp_from_node_name);
+    /* Update node name and/or port if required */
+    ti_node_upd_node(node, from_node_port, &mp_from_node_name);
 
     if (mp_sbuffer_alloc_init(
             &buffer,
@@ -371,7 +362,7 @@ static void nodes__on_req_connect(ti_stream_t * stream, ti_pkg_t * pkg)
 
     (void) ti_node_status_to_pk(this_node, &pk);
 
-send:
+done:
     resp = (ti_pkg_t *) buffer.data;
     pkg_init(resp, pkg->id, TI_PROTO_NODE_RES_CONNECT, buffer.size);
 

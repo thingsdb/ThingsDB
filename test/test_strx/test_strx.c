53354e70e53daf62bd412ca7c3975dfce025555d
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-07-09 17:37:20 +0200

    Update test

--- test/test_strx/test_strx.c
+++ test/test_strx/test_strx.c
@@ -5,21 +5,21 @@
 static int test_strx_to_double(void)
 {
     test_start("strx (to_double)");
-    _assert (strx_to_double("0.5") == 0.5);
-    _assert (strx_to_double("0.55") == 0.55);
-    _assert (strx_to_double("123.456") == 123.456);
-    _assert (strx_to_double("123") == 123);
-    _assert (strx_to_double("1234") == 1234);
-    _assert (strx_to_double("123.") == 123);
-    _assert (strx_to_double("+1234") == 1234);
-    _assert (strx_to_double("-1234") == -1234);
-    _assert (strx_to_double("-0.5") == -0.5);
-    _assert (strx_to_double("-0.56") == -0.56);
-    _assert (strx_to_double("+0.5") == 0.5);
-    _assert (strx_to_double("+0.56") == 0.56);
-    _assert (strx_to_double("-.5") == -0.5);
-    _assert (strx_to_double("+.5") == 0.5);
-    _assert (strx_to_double(".5") == 0.5);
+    _assert (strx_to_double("0.5", NULL) == 0.5);
+    _assert (strx_to_double("0.55", NULL) == 0.55);
+    _assert (strx_to_double("123.456", NULL) == 123.456);
+    _assert (strx_to_double("123", NULL) == 123);
+    _assert (strx_to_double("1234", NULL) == 1234);
+    _assert (strx_to_double("123.", NULL) == 123);
+    _assert (strx_to_double("+1234", NULL) == 1234);
+    _assert (strx_to_double("-1234", NULL) == -1234);
+    _assert (strx_to_double("-0.5", NULL) == -0.5);
+    _assert (strx_to_double("-0.56", NULL) == -0.56);
+    _assert (strx_to_double("+0.5", NULL) == 0.5);
+    _assert (strx_to_double("+0.56", NULL) == 0.56);
+    _assert (strx_to_double("-.5", NULL) == -0.5);
+    _assert (strx_to_double("+.5", NULL) == 0.5);
+    _assert (strx_to_double(".5", NULL) == 0.5);
     return test_end();
 }
 

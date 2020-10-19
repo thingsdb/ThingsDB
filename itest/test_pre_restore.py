4e9a816e8254e37065000e367a49f6f4ac04691d
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-10-19 17:18:04 +0200

    Added NUM_NODES tot test_pre_restore.py

--- itest/test_pre_restore.py
+++ itest/test_pre_restore.py
@@ -8,6 +8,7 @@ Example restore:
 
 restore('/home/joente/Downloads/playground_20200903040003.tar.gz', true);
 """  # nopep8
+import os
 import asyncio
 from lib import run_test
 from lib import default_test_setup
@@ -17,19 +18,27 @@ from thingsdb.exceptions import NodeError
 from lib.vars import THINGSDB_KEEP_ON_ERROR
 
 
+NUM_NODES = int(os.getenv("NUM_NODES", 2))
+
+assert 1 <= NUM_NODES <= 3
+
 class TestNodes(TestBase):
 
     title = 'Test prepare for restore'
 
-    @default_test_setup(num_nodes=2, seed=2)
+    @default_test_setup(num_nodes=NUM_NODES, seed=2)
     async def run(self):
         await self.node0.init_and_run()
 
         client = await get_client(self.node0)
         await client.del_collection('stuff')
 
-        await self.node1.join_until_ready(client)
+        if NUM_NODES > 1:
+            await self.node1.join_until_ready(client)
 
+        if NUM_NODES > 2:
+            await self.node2.join_until_ready(client)
+        
         client.close()
         await client.wait_closed()
         if THINGSDB_KEEP_ON_ERROR:

943b51af4debd996e78aa2e7fa65982ef9a9376b
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-10-26 19:51:33 +0100

    Added TODO to export function

--- inc/ti/fn/fnexport.h
+++ inc/ti/fn/fnexport.h
@@ -1,6 +1,13 @@
 #include <ti/fn/fn.h>
 
-
+/*
+ * TODO: This is an experimental function. Maybe we want to change the export
+ *       and include data as an optional. This could be achieved by moving the
+ *       export function to the @node scope, and schedule an export just like
+ *       a backup. The export(..) function should then export all scopes,
+ *       or defined by a configuration, should accept a Google Storage or file
+ *       path, and should have an import(..) function to read an exported file.
+ */
 static int do__f_export(ti_query_t * query, cleri_node_t * nd, ex_t * e)
 {
     const int nargs = langdef_nd_n_function_params(nd);

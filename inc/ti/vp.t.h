76b39c05c8038c737744f0e79846c94990c248d4
Jeroen van der Heijden <jeroen@transceptor.technology>
2021-03-05 12:50:13 +0100

    Added comments

--- inc/ti/vp.t.h
+++ inc/ti/vp.t.h
@@ -12,7 +12,10 @@ typedef struct ti_vp_s ti_vp_t;
 struct ti_vp_s
 {
     msgpack_packer pk;
-    ti_query_t * query;                 /* may be undefined */
+    ti_query_t * query;                 /* May be undefined;
+                                         * Query must be set when the value
+                                         * packer is used with `options` > 0
+                                         */
 };
 
 #endif  /* TI_VP_T_H_ */

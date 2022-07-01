2055136a2c8bcd451021fc7ebe1e1456c841bf3b
Jeroen van der Heijden <jeroen@cesbit.com>
2022-07-01 13:10:17 +0200

    Improve code

--- inc/ti/fn/fnvmap.h
+++ inc/ti/fn/fnvmap.h
@@ -11,10 +11,8 @@ typedef struct
 static int vmap__walk_i(ti_item_t * item, vmap__walk_i_t * w)
 {
     if (ti_closure_vars_val(w->closure, item->val, w->e) ||
-        ti_closure_do_statement(w->closure, w->query, w->e))
-        return -1;
-
-    if (ti_val_make_variable(&w->query->rval, w->e) ||
+        ti_closure_do_statement(w->closure, w->query, w->e) ||
+        ti_val_make_variable(&w->query->rval, w->e) ||
         !ti_thing_i_item_add(w->thing, item->key, w->query->rval))
         return -1;
 
@@ -51,38 +49,38 @@ static int do__f_vmap(ti_query_t * query, cleri_node_t * nd, ex_t * e)
             ti_closure_inc(closure, query, e))
         goto fail1;
 
-    nthing = ti_thing_is_dict(othing)
-        ? ti_thing_i_create(0, query->collection)
-        : ti_thing_o_create(0, ti_thing_n(othing), query->collection);
-    if (!nthing)
-        goto fail2;
 
-    if (ti_thing_is_object(othing))
+    if (ti_thing_is_dict(othing))
     {
-        if (ti_thing_is_dict(othing))
-        {
-            vmap__walk_i_t w = {
-                    .closure = closure,
-                    .e = e,
-                    .query = query,
-                    .thing = nthing,
-            };
+        vmap__walk_i_t w = {
+                .closure = closure,
+                .e = e,
+                .query = query,
+                .thing = nthing,
+        };
 
-            if (smap_values(
-                    othing->items.smap,
-                    (smap_val_cb) vmap__walk_i,
-                    &w))
-                goto fail2;
-        }
-        else
+        nthing = ti_thing_i_create(0, query->collection);
+
+        if (!nthing || smap_values(
+                othing->items.smap,
+                (smap_val_cb) vmap__walk_i,
+                &w))
+            goto fail2;
+    }
+    else
+    {
+        nthing = ti_thing_o_create(0, ti_thing_n(othing), query->collection);
+        if (!nthing)
+            goto fail2;
+
+        if (ti_thing_is_object(othing))
         {
+            /* the original thing is an object */
             for (vec_each(othing->items.vec, ti_prop_t, prop))
             {
                 if (ti_closure_vars_val(closure, prop->val, e) ||
-                    ti_closure_do_statement(closure, query, e))
-                    goto fail2;
-
-                if (ti_val_make_variable(&query->rval, e) ||
+                    ti_closure_do_statement(closure, query, e) ||
+                    ti_val_make_variable(&query->rval, e) ||
                     !ti_thing_p_prop_add(nthing, prop->name, query->rval))
                     goto fail2;
 
@@ -90,23 +88,22 @@ static int do__f_vmap(ti_query_t * query, cleri_node_t * nd, ex_t * e)
                 query->rval = NULL;
             }
         }
-    }
-    else
-    {
-        ti_name_t * name;
-        ti_val_t * val;
-        for (thing_t_each(othing, name, val))
+        else
         {
-            if (ti_closure_vars_val(closure, val, e) ||
-                ti_closure_do_statement(closure, query, e))
-                goto fail2;
+            /* the original thing is an instance */
+            ti_name_t * name;
+            ti_val_t * val;
+            for (thing_t_each(othing, name, val))
+            {
+                if (ti_closure_vars_val(closure, val, e) ||
+                    ti_closure_do_statement(closure, query, e) ||
+                    ti_val_make_variable(&query->rval, e) ||
+                    !ti_thing_p_prop_add(nthing, name, query->rval))
+                    goto fail2;
 
-            if (ti_val_make_variable(&query->rval, e) ||
-                !ti_thing_p_prop_add(nthing, name, query->rval))
-                goto fail2;
-
-            ti_incref(name);
-            query->rval = NULL;
+                ti_incref(name);
+                query->rval = NULL;
+            }
         }
     }
 

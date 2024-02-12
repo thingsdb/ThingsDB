87d7fccbf504bc38b1a0bd6e28789c746992f5fa
Jeroen van der Heijden <jeroen@cesbit.com>
2024-02-12 10:16:56 +0100

    inline limit

--- Release/src/ti/mod/subdir.mk
+++ Release/src/ti/mod/subdir.mk
@@ -26,7 +26,7 @@ C_DEPS += \
 src/ti/mod/%.o: ../src/ti/mod/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -std=gnu11 -DNDEBUG -I../inc -O3 -Wall -Wextra $(CFLAGS) -c -fmessage-length=0 -msse4.2 -finline-limit=4000 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -std=gnu11 -DNDEBUG -I../inc -O3 -Wall -Wextra $(CFLAGS) -c -fmessage-length=0 -msse4.2 -finline-limit=5000 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

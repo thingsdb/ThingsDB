aab403457996df23789da8f77866c8ad4a58d2b5
Jeroen van der Heijden <jeroen@cesbit.com>
2024-04-09 11:59:16 +0200

    Update build scripts

--- debug-build.sh
+++ debug-build.sh
@@ -1,1 +1,4 @@
-cmake -DCMAKE_BUILD_TYPE=Debug . && make
+#!/bin/bash
+cd "$(dirname "$0")"
+cmake -DCMAKE_BUILD_TYPE=Debug .
+make
--- release-build.sh
+++ release-build.sh
@@ -1,1 +1,4 @@
-cmake -DCMAKE_BUILD_TYPE=Release . && make
+#!/bin/bash
+cd "$(dirname "$0")"
+cmake -DCMAKE_BUILD_TYPE=Release .
+make

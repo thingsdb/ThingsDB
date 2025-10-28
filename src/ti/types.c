75555c50892196af22e44d6e0f0abccf99f43fa8
Jeroen van der Heijden <jeroen@cesbit.com>
2025-10-28 09:51:34 +0100

    missing )

--- src/ti/types.c
+++ src/ti/types.c
@@ -84,7 +84,9 @@ int types__spec_flags_pos(const unsigned char * x)
            x[i] == '&' ||
            x[i] == '-' ||
            x[i] == '+' ||
-           x[i] == '*')
+           x[i] == '*' ||
+           x[i] == '?' ||
+           x[i] == '!')
        i++;
     return i;
 }

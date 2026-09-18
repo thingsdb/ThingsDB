744ff7538699608cba4c009873059567c0d27f5d
Jeroen van der Heijden <jeroen@cesbit.com>
2026-09-18 21:20:37 +0200

    work on dict

--- inc/ti/dict.inline.h
+++ inc/ti/dict.inline.h
@@ -25,6 +25,43 @@ static inline _Bool ti_dict_is_stored(ti_dict_t * dict)
     return dict->parent && dict->parent->id;
 }
 
-static inline int ti_dict_set(ti_dict_t * dict, ti_val_t * key, ti_val_t * val, ex_t * e)
+static inline void * ti_vset_key(ti_vset_t * vset)
+{
+    return ti_thing_is_object(vset->parent)
+            ? vset->key_
+            : ((ti_field_t *) vset->key_)->name;
+}
+
+
+static inline int ti_dict_set(ti_dict_t * dict,
+                              ti_val_t * key,
+                              ti_val_t * val,
+                              ex_t * e)
+{
+    switch((ti_val_enum) key->tp)
+    {
+        case TI_VAL_UUID:
+            if (ti_dict_set_uuid(dict, VUUID(key), val))
+                ex_set_mem(e);
+            break;
+        case TI_VAL_INT:
+            if (ti_dict_set_int(dict, VINT(key), val))
+                ex_set_mem(e);
+            break;
+        case TI_VAL_NAME:
+        case TI_VAL_STR:
+        {
+            ti_str_t * str = (ti_str_t *) key;
+            if (ti_dict_set_strn(dict, str->str, str->n, val))
+                ex_set_mem(e);
+            break;
+        }
+        default:
+            ex_set(e, EX_TYPE_ERROR,
+                "cannot use type `%s` as a dictionary key",
+                ti_val_str(key));
+    }
+    return e->nr;
+}
 
 #endif  /* TI_DICT_INLINE_H_ */
